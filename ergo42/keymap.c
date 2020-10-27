1c39a76b03d001528a01b0d9703e96ac096e986b
Kimura Kazuyuki <kimura@KIMURAK-M4.local>
2020-10-27 15:48:42 +0900

    fix TILD and GRV

--- ergo42/keymap.c
+++ ergo42/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT_NUMB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    S(KC_9),          S(KC_0),    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_MINS, \
     KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,          KC_RBRC,    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    S(KC_LBRC),       S(KC_RBRC), KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
-    LT_MOUS,  KC_LGUI, KC_LALT, KC_BSPC, KC_SPC,  LT_CRSR, KC_ENT,           KC_BSPC,    LT_CRSR, KC_ENT, KC_RCTRL, KC_RALT,  KC_RGUI,  LT_FUNC  \
+    LT_MOUS,  KC_LGUI, KC_LALT, LT_FUNC, KC_BSPC, LT_CRSR, KC_ENT,           KC_BSPC,    LT_CRSR, KC_ENT, KC_RCTRL, KC_RALT,  KC_RGUI,  LT_FUNC  \
   ),
 
   /* LY_CURSOR
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [LY_FUNC] = LAYOUT( \
     KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,   KC_AMPR,  KC_ASTR,   KC_LPRN,   KC_RPRN, KC_UNDS, KC_PLUS, KC_PIPE, \
-  S(KC_TILD), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,     KC_8,      KC_9,      KC_0,    KC_MINS, KC_EQL,  KC_BSLS, \
+    KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,     KC_8,      KC_9,      KC_0,    KC_MINS, KC_EQL,  KC_BSLS, \
     _______,  _______, _______, _______, _______, _______, _______,   _______,  _______,   _______,   _______, _______, _______, _______, \
     _______,  _______, _______, _______, _______, _______, _______,   _______,  _______,   _______,   _______, _______, _______, _______  \
   )
