6b30751e486949094875ad3cf4b1fa6b1ab77de1
tecokimura <tecokimura@gmail.com>
2023-02-10 13:00:10 +0900

    update for hhkb keymaps

--- README.md
+++ README.md
@@ -2,28 +2,49 @@
 自作キーボードのキーマップ定義  
 qmkインストールしてすぐ使えるようにkeymapだけ定義している。  
 
-# How to setup  
+# How to setup for QMK  
 https://beta.docs.qmk.fm/  
 を基にインストールを行う。  
+Mac だとCPUによって準備が異なるので注意
 
-## Install  
-### for Mac  
-$ brew install qmk/qmk/qmk  
+## 準備
+for Mac M1
+https://blog.looseknot.jp/mac/m1mac_terminal_rosetta.html
+https://zenn.dev/bon/scraps/1cd6c49320a0ff
 
-### for Windows  
-You have to use installer  
+## QMK setup
+https://docs.qmk.fm/#/newbs_getting_started
+
+## make link
+$ git clone ~/Documents/hoge/{THIS_REPOSITORY}
+$ ln -s ~/Documents/hoge/qmk-keymaps/hhkb ~/qmk_firmware/keyboards/hhkb/jp/keymaps/tecokimura
+
+## Keycode
+https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes_basic.md
 
 ## New keymap  
 $ qmk new-keymap -kb mint60 -km tecokimura  
+$ qmk new-keymap -kb hhkb/jp -km tecokimura  
 
 ## Compile keymap  
 $ qmk compile -kb mint60 -km tecokimura  
 $ qmk compile -kb helix/rev2 -km mykeymap   
-  
+$ qmk compile -kb hhkb/jp -km tecokimura   
+
+## 解説
+-kb オプションって何を渡せばいいの？
+qmk_firmware/keyboards/**/keymaps/
+の**の箇所、keyboardsとkeymapsの間のパス？です。
+自分の自作したキーボードか反映させるキーボードがどれに当たるかぐぐって調べましょう
+
+-km は？
+keymaps/の下のフォルダ名です。
+
+QMKのversion更新で多少変わることがあるかもなので確認しましょう。
+
 ## Copy keymaps  
 ### mint60  
 ~/qmk_firmware/keyboards/mint60/keymaps/tecokimura/  
-に入れるもの  
 
 ### ergo42  
 ~/qmk_firmware/keyboards/ergo42/keymaps/tecokimura/  
--- hhkb/keymap.c
+++ hhkb/keymap.c
@@ -1,7 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#include "keymap_jp.h"
-
 #define LY_NRML  0
 #define LY_CRSR  1
 #define LY_NUMB  2
@@ -19,6 +17,9 @@
 #define MOUSEKEY_MAX_SPEED   70
 #define MOUSEKEY_TIME_TO_MAX 20
 
+#define JP_EISU KC_INT5
+#define JP_HIRA KC_INT4
+#define JP_BSLS KC_INT3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -35,33 +36,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Fn ||Alt|Cmd|Mhen|Bsp|  Spc  |Ent|Henk|Alt|Sft||Lft|Dwn|Rgh|
      * `-----------------------------------------------------------'
      */
-    [LY_NRML] = LAYOUT_JP(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRAVE,
+    [LY_NRML] = LAYOUT_jp(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL,  JP_BSLS, KC_GRAVE,
         LT_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,  KC_RBRC, // KC_ENT
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,  KC_ENT,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RO,    KC_UP,   LT_RFN,
-        LT_LFN,  KC_LALT, KC_LGUI, KC_MHEN, KC_BSPC,      LT_SPC,     KC_ENT,  KC_HENK, KC_RALT,KC_RSFT, KC_LEFT,  KC_DOWN, KC_RGHT),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,  KC_UP,   LT_RFN,
+        LT_LFN,  KC_LALT, KC_LGUI, JP_EISU, KC_BSPC,      LT_SPC,     KC_ENT,  JP_HIRA, KC_RALT,KC_RSFT, KC_LEFT,  KC_DOWN, KC_RGHT),
 
 
     /* Layer LY_CRSR: with SPACE
      * ,-----------------------------------------------------------.
-     * |`  |   |   |   |   |   |   |   |   |   |Hme|  -|  =|   |  `|
+     * |ESC|   |   |   |   |   |   |   |   |   |Hme|  -|  =|   |   |
      * |-----------------------------------------------------------|
      * |     |   |   |End|   |   |Hom|Pdn|Pup|   |   |   |   |     |
      * |------------------------------------------------------`    |
      * |Ctrl  |Hom|Spc|Del|   |   |Lft|Dwn| Up|Rgh|End|   |   |    |
      * |-----------------------------------------------------------|
-     * |Shft   |   |   |   |   |Bsp|Ent|   |   |   |   |   | Up|Sft|
+     * |Shft|Undo|Cut|Copy|Past|Bsp|Ent|   |   |   |   |   | Up|   |
      * |-----------------------------------------------------------|
-     * |Fn ||Alt|Cmd|Mhen|Bsp|  Spc  |Ent|Henk|Alt|Sft||Lft|Dwn|Rgh|
+     * |Fn | Alt|Cmd|Mhen|Bsp|  Spc  |Ent|Henk|Alt|Sft||Lft|Dwn|Rgh|
      * `-----------------------------------------------------------'
      */
-    [LY_CRSR] = LAYOUT_JP(
-        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  _______, KC_GRAVE,
+    [LY_CRSR] = LAYOUT_jp(
+        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  _______, _______,
         _______, _______, _______, KC_END,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, _______, _______, _______, _______,
         KC_LCTL, KC_HOME, KC_SPC,  KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,  _______, _______, _______,
-        KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,KC_BSPC, KC_ENT,  _______, _______, _______, _______, _______, KC_UP,   KC_RSFT,
-        _______,  KC_LALT, KC_LGUI, KC_MHEN, KC_BSPC,      KC_SPC,      KC_ENT,  KC_HENK, KC_RALT, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,KC_BSPC, KC_ENT,  _______, _______, _______, _______, _______, KC_UP,   XXXXXXX,
+        XXXXXXX,  KC_LALT, KC_LGUI, JP_EISU, KC_BSPC,      KC_SPC,      KC_ENT,  JP_HIRA, KC_RALT, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT),
 
 
     /* Layer LY_NUMB: with TAB
@@ -77,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |   ||   |   |    |   | Space |   |    |   |   ||   |   |   |
      * `-----------------------------------------------------------'
      */
-    [LY_NUMB] = LAYOUT_JP(
+    [LY_NUMB] = LAYOUT_jp(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, KC_EQL,  _______, KC_TAB,  _______, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_SLSH, _______,
         KC_LCTL, _______, KC_SPC,  KC_DEL,  _______, _______, KC_0,    KC_4,    KC_5,    KC_6,    KC_PLUS, KC_MINS, _______, _______,
@@ -99,27 +100,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     // with LEFT FN
-    [LY_LFNC] = LAYOUT_JP(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,
+    [LY_LFNC] = LAYOUT_jp(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_BSPC,
         KC_TAB,  _______, _______, KC_EJCT, _______, KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, _______,
         KC_LCTL, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______, _______,
         KC_LSFT, _______, _______, KC_CAPS, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, KC_BRID, KC_VOLU, KC_BRIU,
         _______, _______, _______, _______, _______,       KC_MPLY,    _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT),
 
     // with RIGHT FN
-    [LY_RFNC] = LAYOUT_JP(
+    [LY_RFNC] = LAYOUT_jp(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,
         KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   _______,
-        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______, _______,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, _______, _______, _______,
+        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, KC_PGUP, _______,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, KC_PGDN, _______, _______,
         _______, _______, _______, _______, _______,       _______,    _______, _______, _______, KC_RSFT, _______, _______, _______)
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t macro_id, uint8_t opt)
-{
-    return MACRO_NONE;
-}
-
-const uint16_t PROGMEM fn_actions[] = {
-
 };
