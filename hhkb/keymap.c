51be0af5cb9b2eae47e1f72ca66b82238a54cf79
tecokimura <tecokimura@gmail.com>
2023-02-14 19:37:10 +0900

    little update and Backslash on OS X

--- hhkb/keymap.c
+++ hhkb/keymap.c
@@ -20,6 +20,7 @@
 #define JP_EISU KC_INT5
 #define JP_HIRA KC_INT4
 #define JP_BSLS KC_INT3
+#define OSXBSLS A(KC_BSLS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -37,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [LY_NRML] = LAYOUT_jp(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL,  JP_BSLS, KC_GRAVE,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRAVE,
         LT_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,  KC_RBRC, // KC_ENT
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,  KC_ENT,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,  KC_UP,   LT_RFN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, OSXBSLS,  KC_UP,   LT_RFN,
         LT_LFN,  KC_LALT, KC_LGUI, JP_EISU, KC_BSPC,      LT_SPC,     KC_ENT,  JP_HIRA, KC_RALT,KC_RSFT, KC_LEFT,  KC_DOWN, KC_RGHT),
 
 
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [LY_CRSR] = LAYOUT_jp(
-        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  _______, _______,
+        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_MINS, KC_EQL,  _______, KC_B,
         _______, _______, _______, KC_END,  _______, _______, KC_HOME, KC_PGDN, KC_PGUP, _______, _______, _______, _______,
         KC_LCTL, KC_HOME, KC_SPC,  KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,  _______, _______, _______,
         KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE,KC_BSPC, KC_ENT,  _______, _______, _______, _______, _______, KC_UP,   XXXXXXX,
